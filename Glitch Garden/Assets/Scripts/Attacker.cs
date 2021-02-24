b809fce06cce2f78a51daba0399f30392175340f
AnastasiaValishina <azhidenova@gmail.com>
2021-02-24 23:41:39 +0200

    attacking state added

--- Glitch Garden/Assets/Scripts/Attacker.cs
+++ Glitch Garden/Assets/Scripts/Attacker.cs
@@ -6,7 +6,13 @@ using UnityEngine;
 public class Attacker : MonoBehaviour
 {
     float currentSpeed = 0f;
+    GameObject currentTarget;
+    //Animator animator;
 
+    private void Start()
+    {
+       // animator = GetComponent<Animator>();
+    }
     void Update()
     {
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
@@ -15,5 +21,11 @@ public class Attacker : MonoBehaviour
     public void SetMovementSpeed(float speed)
     {
         currentSpeed = speed;
+    }
+
+    public void Attack(GameObject target)
+    {
+        GetComponent<Animator>().SetBool("isAttacking", true);
+        currentTarget = target;
     }
 }
