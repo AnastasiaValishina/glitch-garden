c47421e599ccc6fc02e7e2257904665548065925
AnastasiaValishina <azhidenova@gmail.com>
2021-02-25 18:38:00 +0200

    spawning random attacker

--- Glitch Garden/Assets/Scripts/AttackerSpawner.cs
+++ Glitch Garden/Assets/Scripts/AttackerSpawner.cs
@@ -7,7 +7,7 @@ public class AttackerSpawner : MonoBehaviour
 {
     [SerializeField] float minSpawnDelay = 1f;
     [SerializeField] float maxSpawnDelay = 5f;
-    [SerializeField] Attacker attackerPrefab;
+    [SerializeField] Attacker[] attackerPrefabArray;
 
     bool spawn = true;
     
@@ -22,12 +22,14 @@ public class AttackerSpawner : MonoBehaviour
 
     private void SpawnAttacker()
     {
-        Attacker newAttacker = Instantiate(attackerPrefab, transform.position, transform.rotation) as Attacker;
+        int attackerIndex = UnityEngine.Random.Range(0, attackerPrefabArray.Length);
+        Spawn(attackerPrefabArray[attackerIndex]);
+    }
+
+    private void Spawn(Attacker attacker)
+    {
+        Attacker newAttacker = Instantiate(attacker, transform.position, transform.rotation) as Attacker;
         newAttacker.transform.parent = transform;
     }
 
-    void Update()
-    {
-        
-    }
 }
