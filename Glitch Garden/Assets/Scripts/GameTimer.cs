fa81635a465e16d8105a7e5fabdeaaae12ff02e4
AnastasiaValishina <azhidenova@gmail.com>
2021-02-25 22:56:56 +0200

    ui slider

---
+++ Glitch Garden/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [Tooltip("Level duration in seconds")]
+    [SerializeField] float levelDuration = 20f;
+
+    void Update()
+    {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / levelDuration;
+
+        bool timerFinished = (Time.timeSinceLevelLoad >= levelDuration);
+
+        if (timerFinished)
+        {
+            Debug.Log("Times up!");
+        }
+    }
+}
