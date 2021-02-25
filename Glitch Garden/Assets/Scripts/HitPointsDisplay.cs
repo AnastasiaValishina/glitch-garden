a35ab49dcf9db97f790cfd9a984f648ad61ab983
AnastasiaValishina <azhidenova@gmail.com>
2021-02-25 21:37:30 +0200

    hitpoints system added

---
+++ Glitch Garden/Assets/Scripts/HitPointsDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HitPointsDisplay : MonoBehaviour
+{
+    [SerializeField] int hitpoints = 10;
+    [SerializeField] int damage = 1;
+    Text hitpointsText;
+    void Start()
+    {
+        hitpointsText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+
+
+    private void UpdateDisplay()
+    {
+        hitpointsText.text = hitpoints.ToString();
+    }
+
+    public void RemoveHitpoint()
+    {
+        hitpoints -= damage;
+        UpdateDisplay();
+
+        if (hitpoints <= 0)
+        {
+            FindObjectOfType<LevelLoader>().LoadGameOver();
+        }
+    }
+}
